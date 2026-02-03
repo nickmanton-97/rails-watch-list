d9ab5d06235ca9b0bafa590f080b911c419181f7
Nick Manton <mantonnick@outlook.com>
2026-02-03 20:52:57 +1100

    done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@rails/request.js"
 import * as bootstrap from "bootstrap"
-import "@rails/ujs"
 import "@popperjs/core"  // optional
